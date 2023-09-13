5db8316ec4084dd33bb1d173590fe6eba4026cb5
William <william.hardy.abeloos@gmail.com>
2023-09-13 18:00:47 +0200

    first commit

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,8 @@
+import Image from 'next/image'
+import {Button} from '@/components/ui/button'
+
+export default function DashboardPage() {
+    return (
+        <p>Dashboard page (protected)</p>
+    )
+}
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,5 @@
+export default function Page() {
+    return (
+        <div>Landing Page (unprotected)</div>
+    )
+}
