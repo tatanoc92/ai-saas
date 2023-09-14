068dd70663a0f95dc934c73ef65e38ffb5686370
William <william.hardy.abeloos@gmail.com>
2023-09-14 11:55:13 +0200

    set up server auth

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,5 +1,24 @@
-export default function Page() {
+import {getServerSession} from "next-auth/next";
+import {authOptions} from "@/app/api/auth/[...nextauth]/route";
+
+export default async function Page() {
+
+    const session = await getServerSession(authOptions);
+
     return (
-        <div>Landing Page (unprotected)</div>
-    )
+        <>
+            <div>Landing Page (unprotected)</div>
+
+            {session && (
+                <>
+                    <div>Signed in as {session.user && session.user.name}</div>
+                    <a href={'/api/auth/signeout'}>Sign out</a></>
+            )
+            }
+            {
+                !session && (
+                    <p>Not signed in</p>
+                )
+            }
+        </>)
 }
